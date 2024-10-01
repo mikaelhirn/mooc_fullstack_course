dfdb5e046b5bc6c930a37cdae6fee716f96b4a53
Mikael Hirn <mikael.hirn@gmail.com>
2024-10-01 22:26:15 +0300

    2.11 json-server implemented.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import ListContacts from './ListContacts'
 import PersonForm from './PersonForm'
 import Filter from './Filter'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then((response) => {
+      console.log('Promise fullfilled..', response.data)
+      setPersons(response.data)
+    })
+  }, [])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
